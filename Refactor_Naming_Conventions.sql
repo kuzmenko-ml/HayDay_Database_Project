f0d814d9228e6002781bb70702dab635cc07f251
fesqia <se23ex-02@stu.zsea.edu.ua>
2026-04-28 15:46:49 +0300

    add file for refactor naming conventions, renamed smth

---
+++ Refactor_Naming_Conventions.sql
@@ -0,0 +1,6 @@
+USE HayDay_Farm;
+GO
+
+EXEC sp_rename 'Dim_Pets.PetsID', 'PetID', 'COLUMN';
+EXEC sp_rename 'Dim_Pets.PetsName', 'PetName', 'COLUMN';
+EXEC sp_rename 'Dim_Products.ProductsName', 'ProductName', 'COLUMN';
