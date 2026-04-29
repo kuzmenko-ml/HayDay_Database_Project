0f14014c8e2b654c6317a7c86ebfcf2cebfd52d7
fesqia <se23ex-02@stu.zsea.edu.ua>
2026-04-29 11:53:51 +0300

    removed SpotQuantity and rename smth to better, add drop logic

--- 19_Dim_Town_Buildings.sql
+++ 19_Dim_Town_Buildings.sql
@@ -1,9 +1,12 @@
 USE HayDay_Farm;
 GO
 
+IF OBJECT_ID('Dim_Town_Buildings', 'U') IS NOT NULL
+    DROP TABLE Dim_Town_Buildings;
+GO
+
 CREATE TABLE Dim_Town_Buildings(
-TownBuildingID INT PRIMARY KEY IDENTITY(1,1),
-TownBuildingName NVARCHAR(35) NOT NULL,
-TownBuildingRequiredReputation INT NOT NULL,
-TownBuildingSpotQuantity INT NOT NULL DEFAULT 1,
-LocationID INT NOT NULL);
+    TownBuildingID INT PRIMARY KEY IDENTITY(1,1),
+    TownBuildingName NVARCHAR(35) NOT NULL,
+    TownBuildingRequiredReputation INT NOT NULL, 
+    LocationID INT NOT NULL);
