26cb4f82e9d40c7f2fc44e39785236c2d305d2cc
fesqia <se23ex-02@stu.zsea.edu.ua>
2026-04-29 10:26:59 +0300

    add LocationID in table Dim_Buildings

--- 07_Dim_Buildings.sql
+++ 07_Dim_Buildings.sql
@@ -1,7 +1,12 @@
 USE HayDay_Farm;
 GO
 
+IF OBJECT_ID('Dim_Buildings', 'U') IS NOT NULL
+    DROP TABLE Dim_Buildings;
+GO
+
 CREATE TABLE Dim_Buildings(
-BuildingID INT PRIMARY KEY IDENTITY(1,1),
-BuildingName NVARCHAR(35) NOT NULL,
-BuildingRequiredLevel INT NOT NULL);
+    BuildingID INT PRIMARY KEY IDENTITY(1,1),
+    BuildingName NVARCHAR(35) NOT NULL,
+    BuildingRequiredLevel INT NOT NULL,
+    LocationID INT NOT NULL); 
