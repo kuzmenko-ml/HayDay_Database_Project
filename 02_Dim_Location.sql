3c09a692ef690b29fd94584d9b3b8bddf387a98b
fesqia <se23ex-02@stu.zsea.edu.ua>
2026-04-30 12:59:57 +0300

    change RequiredLevel to LocationRequiredLevel

--- 02_Dim_Location.sql
+++ 02_Dim_Location.sql
@@ -1,7 +1,11 @@
 USE HayDay_Farm;
 GO
 
+IF OBJECT_ID('Dim_Location','U') IS NOT NULL
+	DROP TABLE Dim_Location
+GO
+
 CREATE TABLE Dim_Location(
 	LocationID INT PRIMARY KEY IDENTITY(1,1),
 	LocationName NVARCHAR(20) NOT NULL,
-	RequiredLevel INT NOT NULL);
+	LocationRequiredLevel INT NOT NULL);
