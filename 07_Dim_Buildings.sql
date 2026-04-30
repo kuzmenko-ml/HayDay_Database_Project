e12e267799ac2c8a1c3674cc0c06569b3fe7009d
fesqia <se23ex-02@stu.zsea.edu.ua>
2026-04-30 13:14:17 +0300

    add BuildingPrice, ConstructionTimeMinutes

--- 07_Dim_Buildings.sql
+++ 07_Dim_Buildings.sql
@@ -9,4 +9,6 @@ CREATE TABLE Dim_Buildings(
     BuildingID INT PRIMARY KEY IDENTITY(1,1),
     BuildingName NVARCHAR(35) NOT NULL,
     BuildingRequiredLevel INT NOT NULL,
-    LocationID INT NOT NULL); 
+    LocationID INT NOT NULL,
+    BuildingPrice INT NOT NULL,
+    ConstructionTimeMinutes INT NOT NULL); 
