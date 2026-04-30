f1d2582a28aff8b9667799f4cb994d8ffc9310c0
fesqia <se23ex-02@stu.zsea.edu.ua>
2026-04-30 12:57:42 +0300

    add ProductExperience, ProductTimeMinutes, ProductMachineID; rename Price to ProductMaxPrice

--- 05_Dim_Products.sql
+++ 05_Dim_Products.sql
@@ -1,8 +1,15 @@
 USE HayDay_Farm;
 GO
 
+IF OBJECT_ID('Dim_Products','U') IS NOT NULL
+	DROP TABLE Dim_Products
+GO
+
 CREATE TABLE Dim_Products(
 ProductID INT PRIMARY KEY IDENTITY(1,1),
 ProductsName NVARCHAR(25) NOT NULL,
-RequiredLevel INT NOT NULL,
-Price INT NOT NULL);
+ProductRequiredLevel INT NOT NULL,
+ProductMaxPrice INT NOT NULL,
+ProductExperience INT NOT NULL,
+ProductTimeMinutes INT NOT NULL,
+ProductMachineID INT NOT NULL);
