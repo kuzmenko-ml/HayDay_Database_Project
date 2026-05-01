fae5d5be6e1f1b3454a50a499808e364370cf108
fesqia <se23ex-02@stu.zsea.edu.ua>
2026-05-01 16:28:39 +0300

    rename PetsLivestockID to PetLivestockID

--- 10_Fact_Pets_livestock.sql
+++ 10_Fact_Pets_livestock.sql
@@ -1,8 +1,12 @@
 USE HayDay_Farm;
 GO
 
+IF OBJECT_ID('Fact_Pets_Livestock','U') IS NOT NULL
+	DROP TABLE Fact_Pets_Livestock
+GO
+
 CREATE TABLE Fact_Pets_Livestock(
-PetsLivestockID INT PRIMARY KEY IDENTITY(1,1),
+PetLivestockID INT PRIMARY KEY IDENTITY(1,1),
 FarmID INT NOT NULL,
 PetID INT NOT NULL,
 PetQuantity INT NOT NULL);
