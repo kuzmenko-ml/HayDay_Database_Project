0328329c0b1a11cb9ead63bf4c6fef72d0357869
fesqia <se23ex-02@stu.zsea.edu.ua>
2026-05-01 18:18:04 +0300

    made FK (blue line)

--- FK.sql
+++ FK.sql
@@ -99,3 +99,44 @@ ALTER TABLE Fact_Farm_Wallet
 ADD CONSTRAINT FK_FactFarmWallet_DimFarms
 FOREIGN KEY (FarmID) REFERENCES Dim_Farms (FarmID);
 
+-- зв'язки (синя лінія)
+
+ALTER TABLE Fact_Farm_Livestock
+ADD CONSTRAINT FK_FactFarmLivestock_DimAnimals
+FOREIGN KEY (AnimalID) REFERENCES Dim_Animals (AnimalID);
+
+ALTER TABLE Fact_Pets_Livestock
+ADD CONSTRAINT FK_FactPetsLivestock_DimPets
+FOREIGN KEY (PetID) REFERENCES Dim_Pets (PetID);
+
+ALTER TABLE Fact_Barn
+ADD CONSTRAINT FK_FactBarn_DimProducts
+FOREIGN KEY (ProductID) REFERENCES Dim_Products (ProductID);
+
+ALTER TABLE Fact_Silo
+ADD CONSTRAINT FK_FactSilo_DimCrops
+FOREIGN KEY (CropID) REFERENCES Dim_Crops (CropID);
+
+ALTER TABLE Fact_Buildings
+ADD CONSTRAINT FK_FactBuildings_DimBuildings
+FOREIGN KEY (BuildingID) REFERENCES Dim_Buildings (BuildingID);
+
+ALTER TABLE Fact_Tree_Bush
+ADD CONSTRAINT FK_FactTreeBush_DimTreeBush
+FOREIGN KEY (TreeOrBushID) REFERENCES Dim_Tree_Bush (TreeOrBushID);
+
+ALTER TABLE Fact_Town_Buildings
+ADD CONSTRAINT FK_FactTownBuildings_DimTownBuildings
+FOREIGN KEY (TownBuildingID) REFERENCES Dim_Town_Buildings (TownBuildingID);
+
+ALTER TABLE Fact_Tackle_Box
+ADD CONSTRAINT FK_FactTackleBox_DimTackle
+FOREIGN KEY (TackleID) REFERENCES Dim_Tackle (TackleID);
+
+ALTER TABLE Fact_Fishing_Spots
+ADD CONSTRAINT FK_FactFishingSpots_DimFishingSpots
+FOREIGN KEY (SpotID) REFERENCES Dim_Fishing_Spots (SpotID);
+
+ALTER TABLE Fact_Farm_Wallet
+ADD CONSTRAINT FK_FactFarmWallet_DimCurrencies
+FOREIGN KEY (CurrencyID) REFERENCES Dim_Currencies (CurrencyID);
