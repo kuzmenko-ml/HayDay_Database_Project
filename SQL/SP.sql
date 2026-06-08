5906f9f00f442f49d4f2782b942c48fd83d72cab
fesqia <se23ex-02@stu.zsea.edu.ua>
2026-06-08 18:30:23 +0300

    change argument name

--- SQL/SP.sql
+++ SQL/SP.sql
@@ -110,3 +110,57 @@ BEGIN
 		WHERE FarmID = @FarmID AND StorageTypeID = @TempStorageID
 	END
 END;
+
+ALTER PROCEDURE SP_FarmInfoUpdate
+	@FarmID INT,
+	@NewFarmName NVARCHAR(50) = NULL,
+	@NewFarmLevel INT = NULL,
+	@NewFarmExperience INT = NULL,
+	@NewFarmCreatedAt DATE = NULL,
+	@CurrencyName NVARCHAR(50) = NULL,
+	@NewCurrencyQuantity INT = NULL,
+	@StorageType NVARCHAR(50) = NULL,
+	@NewStorageCapacity INT = NULL
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	UPDATE Dim_Farms
+	SET 
+	FarmName = ISNULL(@NewFarmName, FarmName),
+	FarmLevel = ISNULL(@NewFarmLevel,FarmLevel),
+	FarmExperience = ISNULL(@NewFarmExperience,FarmExperience),
+	FarmCreatedAt = ISNULL(@NewFarmCreatedAt,FarmCreatedAt)
+	WHERE FarmId = @FarmID;
+
+	IF @CurrencyName IS NOT NULL AND @NewCurrencyQuantity IS NOT NULL 
+	BEGIN 
+		DECLARE @TempNameID INT
+
+		IF TRIM(UPPER(@CurrencyName)) = 'COINS'
+			SET @TempNameID = 1;
+		ELSE IF TRIM(UPPER(@CurrencyName)) = 'DIAMONDS'
+			SET @TempNameID = 2;
+
+		UPDATE Fact_Farm_Wallet
+		SET
+		CurrencyQuantity = @NewCurrencyQuantity
+		WHERE FarmID = @FarmID AND CurrencyID = @TempNameID;
+	END
+
+	IF @StorageType IS NOT NULL AND @NewStorageCapacity IS NOT NULL
+	BEGIN 
+		DECLARE @TempStorageID INT
+
+		IF TRIM(UPPER(@StorageType)) = 'AMBAR'
+			SET @TempStorageID = 1;
+		ELSE IF TRIM(UPPER(@StorageType)) = 'SILO'
+			SET @TempStorageID = 2;
+		ELSE IF TRIM(UPPER(@StorageType)) = 'TACKLEBOX'
+			SET @TempStorageID = 3;
+
+		UPDATE Dim_Storages
+		SET StorageCapacity = @NewStorageCapacity
+		WHERE FarmID = @FarmID AND StorageTypeID = @TempStorageID
+	END
+END;
