9f854a8434438f9aa97a7601668544f2fd9ac729
fesqia <se23ex-02@stu.zsea.edu.ua>
2026-06-14 14:45:09 +0300

    make view instead of storage procedure SP_AllFarms

--- SQL/DML.sql
+++ SQL/DML.sql
@@ -287,3 +287,16 @@ BEGIN
 	END
 END;
 
+DROP PROCEDURE SP_AllFarms;
+
+CREATE VIEW dbo.vw_AllFarms AS
+SELECT f.FarmName, f.FarmLevel, f.FarmCreatedAt,
+       MAX(CASE WHEN fw.CurrencyID = 1 THEN fw.CurrencyQuantity ELSE 0 END) AS Coins,
+       MAX(CASE WHEN fw.CurrencyID = 2 THEN fw.CurrencyQuantity ELSE 0 END) AS Diamonds,
+       MAX(CASE WHEN s.StorageTypeID = 1 THEN s.StorageCapacity ELSE 0 END) AS Ambar,
+       MAX(CASE WHEN s.StorageTypeID = 2 THEN s.StorageCapacity ELSE 0 END) AS Silo,
+       MAX(CASE WHEN s.StorageTypeID = 3 THEN s.StorageCapacity ELSE 0 END) AS [Tackle box]
+FROM Dim_Farms f
+INNER JOIN Fact_Farm_Wallet fw ON f.FarmID = fw.FarmID
+INNER JOIN Dim_Storages s ON f.FarmID = s.FarmID
+GROUP BY f.FarmName, f.FarmLevel, f.FarmCreatedAt;
