d66ce43b86500b501f81d54f02654a3afaa6fe35
fesqia <se23ex-02@stu.zsea.edu.ua>
2026-06-24 15:00:14 +0300

    refactor(func load_base_dimensions): switch to pathlib for cleaner and cross-platform data paths

--- SCRIPTS/etl_pipeline.py
+++ SCRIPTS/etl_pipeline.py
@@ -6,13 +6,11 @@ from pathlib import Path
 def load_base_dimensions(engine):
     print("Старт завантаження базових довідників...")
     
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    project_root = os.path.dirname(script_dir)
-    data_dir = os.path.join(project_root, 'DATA')
+    data_dir = Path(__file__).resolve().parent.parent / 'DATA'
 
     # 1. Завантажуємо Dim_Farms
     print("Читання farms.csv...")
-    farms_path = os.path.join(data_dir, 'farms.csv')
+    farms_path = data_dir / 'farms.csv'
     df_farms = pd.read_csv(farms_path)
     # назва таблиці на сервері, з'єднання створене(місток),дописування до існуючих даниї,ігнорування індексів пандас
     df_farms.to_sql('Dim_Farms', con=engine, if_exists='append', index=False)
@@ -21,7 +19,7 @@ def load_base_dimensions(engine):
     
     # 2. Завантажуємо Dim_Location
     print("Читання locations.csv...")
-    locations_path = os.path.join(data_dir, 'locations.csv')
+    locations_path = data_dir / 'locations.csv'
     df_locations = pd.read_csv(locations_path)
     df_locations.to_sql('Dim_Location', con=engine, if_exists='append', index=False)
     print("Таблиця Dim_Location успішно заповнена!")
@@ -29,7 +27,7 @@ def load_base_dimensions(engine):
     
     # 3. Завантажуємо Dim_Storage_Type
     print("Читання storage_types.csv...")
-    types_path = os.path.join(data_dir, 'storage_types.csv')
+    types_path = data_dir / 'storage_types.csv'
     df_types = pd.read_csv(types_path)
     df_types.to_sql('Dim_Storage_Type', con=engine, if_exists='append', index=False)
     print("Таблиця Dim_Storage_Type успішно заповнена!")
@@ -37,7 +35,7 @@ def load_base_dimensions(engine):
     
     # 4. ОБРОБКА ТА ЗАВАНТАЖЕННЯ ЗАЛЕЖНОЇ ТАБЛИЦІ Dim_Storages
     print("Обробка складного довідника Dim_Storages...")
-    storages_path = os.path.join(data_dir, 'storages.csv')
+    storages_path = data_dir / 'storages.csv'
     df_storages = pd.read_csv(storages_path) # Текстовий файл (FarmName, StorageTypeName, StorageCapacity)
     
     # Витягуємо з бази актуальні ID, які SQL Server щойно згенерував для ферм та типів
