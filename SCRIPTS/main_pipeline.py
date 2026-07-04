1bb09a394e54f3a686122d47dca51404ea4afd14
fesqia <se23ex-02@stu.zsea.edu.ua>
2026-07-04 10:08:46 +0300

    refactor: add 'self.' to transform_base_dimensions, transform_game_entities, transform_farm_facts for all parts

--- SCRIPTS/main_pipeline.py
+++ SCRIPTS/main_pipeline.py
@@ -6,7 +6,7 @@ class Hay_Day_ETL_pipeline:
         self.SERVER = '.' 
         self.DATABASE = 'HayDay_Farm'  
     
-        self.connection_string = f"mssql+pyodbc://@{SERVER}/{DATABASE}?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
+        self.connection_string = f"mssql+pyodbc://@{self.SERVER}/{self.DATABASE}?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
         self.engine = create_engine(self.connection_string)
     
     def delete_null_data(self, df, columns=None):
@@ -20,7 +20,7 @@ class Hay_Day_ETL_pipeline:
         
     def transform_base_dimensions(self):
         try:
-            df_location = pd.read_sql("SELECT * FROM raw.Dim_Location", engine)
+            df_location = pd.read_sql("SELECT * FROM raw.Dim_Location", self.engine)
 
             df_location = self.delete_null_data(df_location)
             df_location = self.clean_text(df_location, ['LocationName'])
@@ -28,13 +28,13 @@ class Hay_Day_ETL_pipeline:
 
             existing_names = []
             try:
-                df_location_existing = pd.read_sql("SELECT LocationName FROM Dim_Location", engine)
+                df_location_existing = pd.read_sql("SELECT LocationName FROM Dim_Location", self.engine)
                 existing_names = df_location_existing['LocationName'].tolist()
             except:
                 pass
 
             df_location = df_location[~df_location['LocationName'].isin(existing_names)]
-            df_location.to_sql('Dim_Location', con=engine, if_exists='append', index=False)
+            df_location.to_sql('Dim_Location', con=self.engine, if_exists='append', index=False)
         except Exception as e:
             print('Помилка! Завантаження файлу Dim_Location не відбулося.')
 
@@ -42,7 +42,7 @@ class Hay_Day_ETL_pipeline:
         storage_type_ok = True
 
         try:
-            df_farms = pd.read_sql("SELECT * FROM raw.Dim_Farms", engine)
+            df_farms = pd.read_sql("SELECT * FROM raw.Dim_Farms", self.engine)
 
             df_farms = self.delete_null_data(df_farms)
             df_farms = self.clean_text(df_farms, ['FarmName'])
@@ -50,28 +50,28 @@ class Hay_Day_ETL_pipeline:
             df_farms['FarmExperience'] = df_farms['FarmExperience'].astype(int)
             df_farms['FarmCreatedAt'] = pd.to_datetime(df_farms['FarmCreatedAt'])
 
-            df_farms.to_sql('Dim_Farms', con=engine, if_exists='append', index=False)
+            df_farms.to_sql('Dim_Farms', con=self.engine, if_exists='append', index=False)
         except Exception as e:
             print('Помилка! Завантаження файлу Dim_Farms не відбулося.')
             farms_ok = False
 
         try:
-            df_storage_type = pd.read_sql("SELECT * FROM raw.Dim_Storage_Type", engine)
+            df_storage_type = pd.read_sql("SELECT * FROM raw.Dim_Storage_Type", self.engine)
 
             df_storage_type = self.delete_null_data(df_storage_type)
             df_storage_type = self.clean_text(df_storage_type, ['StorageTypeName'])
 
-            df_storage_type.to_sql('Dim_Storage_Type', con=engine, if_exists='append', index=False)
+            df_storage_type.to_sql('Dim_Storage_Type', con=self.engine, if_exists='append', index=False)
         except Exception as e:
             print('Помилка! Завантаження файлу Dim_Storage_Type не відбулося.')
             storage_type_ok = False
 
         if farms_ok and storage_type_ok:
             try:
-                df_storages = pd.read_sql("SELECT * FROM raw.Dim_Storages", engine)
+                df_storages = pd.read_sql("SELECT * FROM raw.Dim_Storages", self.engine)
 
-                db_farms = pd.read_sql("SELECT FarmID, FarmName FROM Dim_Farms", engine)
-                db_storage_types = pd.read_sql("SELECT StorageTypeID, StorageTypeName FROM Dim_Storage_Type", engine) 
+                db_farms = pd.read_sql("SELECT FarmID, FarmName FROM Dim_Farms", self.engine)
+                db_storage_types = pd.read_sql("SELECT StorageTypeID, StorageTypeName FROM Dim_Storage_Type", self.engine) 
 
                 df_storages = self.clean_text(df_storages, ['FarmName', 'StorageTypeName'])
 
@@ -88,7 +88,7 @@ class Hay_Day_ETL_pipeline:
                     'StorageCapacity'
                 ]]
 
-                df_final_storages.to_sql('Dim_Storages', con=engine, if_exists='append', index=False)
+                df_final_storages.to_sql('Dim_Storages', con=self.engine, if_exists='append', index=False)
             except Exception as e:
                 print('Помилка! Завантаження файлу Dim_Storages не відбулося.')  
         else:
@@ -97,10 +97,10 @@ class Hay_Day_ETL_pipeline:
     def transform_game_entities(self): 
         buildings_ok = True
         try:
-            df_buildings = pd.read_sql("SELECT * FROM raw.Dim_Buildings", engine)
+            df_buildings = pd.read_sql("SELECT * FROM raw.Dim_Buildings", self.engine)
             df_buildings = self.delete_null_data(df_buildings)
 
-            db_location = pd.read_sql("SELECT LocationName, LocationID FROM Dim_Location", engine)
+            db_location = pd.read_sql("SELECT LocationName, LocationID FROM Dim_Location", self.engine)
 
             df_buildings = self.clean_text(df_buildings, ['LocationName', 'BuildingName'])
 
@@ -119,7 +119,7 @@ class Hay_Day_ETL_pipeline:
                 'ConstructionTimeMinutes'
             ]]
 
-            df_final_buildings.to_sql('Dim_Buildings', con=engine, if_exists='append', index=False)
+            df_final_buildings.to_sql('Dim_Buildings', con=self.engine, if_exists='append', index=False)
             print('Dim_Buildings завантажено успішно!')
             print('------------------------------------')
         except Exception as e:
@@ -129,8 +129,8 @@ class Hay_Day_ETL_pipeline:
         products_ok = True
         if buildings_ok:
             try:
-                df_products = pd.read_sql("SELECT * FROM raw.Dim_Products", engine)
-                db_buildings = pd.read_sql("SELECT BuildingName, BuildingID FROM Dim_Buildings", engine)
+                df_products = pd.read_sql("SELECT * FROM raw.Dim_Products", self.engine)
+                db_buildings = pd.read_sql("SELECT BuildingName, BuildingID FROM Dim_Buildings", self.engine)
 
                 df_products = self.delete_null_data(df_products)
                 df_products = self.clean_text(df_products, ['ProductName','BuildingName'])
@@ -150,7 +150,7 @@ class Hay_Day_ETL_pipeline:
                     'BuildingID'
                 ]]
 
-                df_final_products.to_sql('Dim_Products', con=engine, if_exists='append',index=False)
+                df_final_products.to_sql('Dim_Products', con=self.engine, if_exists='append',index=False)
                 print('Dim_Products завантажено успішно!')
                 print('------------------------------------')
 
@@ -163,8 +163,8 @@ class Hay_Day_ETL_pipeline:
 
         if products_ok:
             try:
-                df_animals = pd.read_sql("SELECT * FROM raw.Dim_Animals", engine)
-                db_products = pd.read_sql("SELECT ProductName, ProductID FROM Dim_Products", engine)
+                df_animals = pd.read_sql("SELECT * FROM raw.Dim_Animals", self.engine)
+                db_products = pd.read_sql("SELECT ProductName, ProductID FROM Dim_Products", self.engine)
 
                 df_animals = self.delete_null_data(df_animals)
                 df_animals = self.clean_text(df_animals, ['AnimalName'])
@@ -180,7 +180,7 @@ class Hay_Day_ETL_pipeline:
                     'AnimalRequiredLevel'
                 ]]
 
-                df_final_animals.to_sql('Dim_Animals', con=engine, if_exists='append', index=False)
+                df_final_animals.to_sql('Dim_Animals', con=self.engine, if_exists='append', index=False)
                 print('Dim_Animals завантажено успішно!')
                 print('------------------------------------')
             except Exception as e:
@@ -190,7 +190,7 @@ class Hay_Day_ETL_pipeline:
 
         crops_ok = True
         try:
-            df_crops = pd.read_sql("SELECT * FROM raw.Dim_Crops", engine)
+            df_crops = pd.read_sql("SELECT * FROM raw.Dim_Crops", self.engine)
             df_crops = self.delete_null_data(df_crops)
             df_crops = self.clean_text(df_crops, ['CropName'])
             df_crops['CropRequiredLevel'] = df_crops['CropRequiredLevel'].astype(int)
@@ -198,7 +198,7 @@ class Hay_Day_ETL_pipeline:
             df_crops['CropTimeMinutes'] = df_crops['CropTimeMinutes'].astype(int)
             df_crops['CropMaxPrice'] = df_crops['CropMaxPrice'].astype(int)
 
-            df_crops.to_sql('Dim_Crops', con=engine, if_exists='append', index=False)
+            df_crops.to_sql('Dim_Crops', con=self.engine, if_exists='append', index=False)
             print('Dim_Crops завантажено успішно!')
             print('------------------------------------')
         except Exception as e:
@@ -207,9 +207,9 @@ class Hay_Day_ETL_pipeline:
 
         if products_ok and crops_ok:
             try:
-                df_pets = pd.read_sql("SELECT * FROM raw.Dim_Pets", engine)
-                db_products = pd.read_sql("SELECT ProductName, ProductID FROM Dim_Products", engine)
-                db_crops = pd.read_sql("SELECT CropName, CropID FROM Dim_Crops", engine)
+                df_pets = pd.read_sql("SELECT * FROM raw.Dim_Pets", self.engine)
+                db_products = pd.read_sql("SELECT ProductName, ProductID FROM Dim_Products", self.engine)
+                db_crops = pd.read_sql("SELECT CropName, CropID FROM Dim_Crops", self.engine)
 
                 df_pets = df_pets.dropna(subset=['PetName', 'PetRequiredLevel'], how='any')
                 df_pets = df_pets.dropna(subset=['ProductName', 'CropName'], how='all')
@@ -226,7 +226,7 @@ class Hay_Day_ETL_pipeline:
                     'CropID'
                 ]]
 
-                df_final_pets.to_sql('Dim_Pets', con=engine, if_exists='append',index=False)
+                df_final_pets.to_sql('Dim_Pets', con=self.engine, if_exists='append',index=False)
                 print('Dim_Pets завантажено успішно!')
                 print('------------------------------------')
             except Exception as e:
@@ -236,9 +236,9 @@ class Hay_Day_ETL_pipeline:
 
     def transform_farm_facts(self):
         try:
-            df_farm_livestock = pd.read_sql("SELECT * FROM raw.Fact_Farm_Livestock", engine)
-            db_farms = pd.read_sql("SELECT FarmName, FarmID FROM Dim_Farms", engine)
-            db_animals = pd.read_sql("SELECT AnimalName, AnimalID FROM Dim_Animals", engine)
+            df_farm_livestock = pd.read_sql("SELECT * FROM raw.Fact_Farm_Livestock", self.engine)
+            db_farms = pd.read_sql("SELECT FarmName, FarmID FROM Dim_Farms", self.engine)
+            db_animals = pd.read_sql("SELECT AnimalName, AnimalID FROM Dim_Animals", self.engine)
 
             initial_count = len(df_farm_livestock)
 
@@ -261,7 +261,7 @@ class Hay_Day_ETL_pipeline:
                     'AnimalQuantity'
                 ]]
 
-                df_final_farm_livestock.to_sql('Fact_Farm_Livestock', con=engine, if_exists='append', index=False)
+                df_final_farm_livestock.to_sql('Fact_Farm_Livestock', con=self.engine, if_exists='append', index=False)
                 print('Успішно! Частина або всі нові дані Fact_Farm_Livestock додані в БД.')
             print('------------------------------')
         except Exception as e:
@@ -269,9 +269,9 @@ class Hay_Day_ETL_pipeline:
             print('------------------------------')
 
         try:
-            df_pets_livestock = pd.read_sql("SELECT * FROM raw.Fact_Pets_Livestock", engine)
-            db_pets = pd.read_sql("SELECT PetID, PetName FROM Dim_Pets", engine)
-            db_farms = pd.read_sql("SELECT FarmID, FarmName FROM Dim_Farms", engine)
+            df_pets_livestock = pd.read_sql("SELECT * FROM raw.Fact_Pets_Livestock", self.engine)
+            db_pets = pd.read_sql("SELECT PetID, PetName FROM Dim_Pets", self.engine)
+            db_farms = pd.read_sql("SELECT FarmID, FarmName FROM Dim_Farms", self.engine)
 
             initial_count = len(df_pets_livestock)
 
@@ -294,17 +294,17 @@ class Hay_Day_ETL_pipeline:
                     'PetQuantity'
                 ]]
 
-                df_final_pets_livestock.to_sql('Fact_Pets_Livestock', con=engine, if_exists='append',index=False)
+                df_final_pets_livestock.to_sql('Fact_Pets_Livestock', con=self.engine, if_exists='append',index=False)
                 print('Успішно! Fact_Pets_Livestock')
             print('------------------------------')
         except Exception as e:
             print(f'Помилка! Скрипт упав під час обробки Fact_Pets_Livestock. Деталі: {e}')
 
         try:
-            df_barn = pd.read_sql("SELECT * FROM raw.Fact_Barn", engine)
-            db_storages = pd.read_sql("SELECT StorageID, FarmID FROM Dim_Storages WHERE StorageTypeID = 1", engine)
-            db_products = pd.read_sql("SELECT ProductID, ProductName FROM Dim_Products", engine)
-            db_farms = pd.read_sql("SELECT FarmName, FarmID FROM Dim_Farms", engine)
+            df_barn = pd.read_sql("SELECT * FROM raw.Fact_Barn", self.engine)
+            db_storages = pd.read_sql("SELECT StorageID, FarmID FROM Dim_Storages WHERE StorageTypeID = 1", self.engine)
+            db_products = pd.read_sql("SELECT ProductID, ProductName FROM Dim_Products", self.engine)
+            db_farms = pd.read_sql("SELECT FarmName, FarmID FROM Dim_Farms", self.engine)
 
             initial_count = len(df_barn)
 
@@ -329,17 +329,17 @@ class Hay_Day_ETL_pipeline:
                     'ProductCount'
                 ]]
 
-                df_final_barn.to_sql('Fact_Barn', con=engine, if_exists='append',index=False)
+                df_final_barn.to_sql('Fact_Barn', con=self.engine, if_exists='append',index=False)
                 print('Успішно! Fact_Barn')
             print('------------------------------')
         except Exception as e:
             print(f'Помилка! Скрипт упав під час обробки Fact_Barn. Деталі: {e}')
 
         try:
-            df_silo = pd.read_sql("SELECT * FROM raw.Fact_Silo", engine)
-            db_storages = pd.read_sql("SELECT StorageID, FarmID FROM Dim_Storages WHERE StorageTypeID = 2", engine)
-            db_crops = pd.read_sql("SELECT CropID, CropName FROM Dim_Crops", engine)
-            db_farms = pd.read_sql("SELECT FarmName, FarmID FROM Dim_Farms", engine)
+            df_silo = pd.read_sql("SELECT * FROM raw.Fact_Silo", self.engine)
+            db_storages = pd.read_sql("SELECT StorageID, FarmID FROM Dim_Storages WHERE StorageTypeID = 2", self.engine)
+            db_crops = pd.read_sql("SELECT CropID, CropName FROM Dim_Crops", self.engine)
+            db_farms = pd.read_sql("SELECT FarmName, FarmID FROM Dim_Farms", self.engine)
 
             initial_count = len(df_silo)
 
@@ -364,17 +364,17 @@ class Hay_Day_ETL_pipeline:
                     'CropCount'
                 ]]
 
-                df_final_silo.to_sql('Fact_Silo', con=engine, if_exists='append',index=False)
+                df_final_silo.to_sql('Fact_Silo', con=self.engine, if_exists='append',index=False)
                 print('Успішно! Fact_Silo')
             print('------------------------------')
         except Exception as e:
             print(f'Помилка! Скрипт упав під час обробки Fact_Silo. Деталі: {e}')
 
         try:
-            df_buildings = pd.read_sql("SELECT * FROM raw.Fact_Buildings", engine)
-            db_buildings = pd.read_sql("SELECT BuildingName, BuildingID FROM Dim_Buildings", engine)
-            db_location = pd.read_sql("SELECT LocationName, LocationID FROM Dim_Location", engine)
-            db_farms = pd.read_sql("SELECT FarmName, FarmID FROM Dim_Farms", engine)
+            df_buildings = pd.read_sql("SELECT * FROM raw.Fact_Buildings", self.engine)
+            db_buildings = pd.read_sql("SELECT BuildingName, BuildingID FROM Dim_Buildings", self.engine)
+            db_location = pd.read_sql("SELECT LocationName, LocationID FROM Dim_Location", self.engine)
+            db_farms = pd.read_sql("SELECT FarmName, FarmID FROM Dim_Farms", self.engine)
 
             initial_count = len(df_buildings)
 
@@ -401,27 +401,9 @@ class Hay_Day_ETL_pipeline:
                     'MasteryStars'
                 ]]
 
-                df_final_buildings.to_sql('Fact_Buildings', con=engine, if_exists='append',index=False)
+                df_final_buildings.to_sql('Fact_Buildings', con=self.engine, if_exists='append',index=False)
                 print('Успішно! Fact_Buildings')
             print('------------------------------')
         except Exception as e:
             print(f'Помилка! Скрипт упав під час обробки Fact_Buildings. Деталі: {e}')
 
-
-if __name__ == "__main__":
-    SERVER = '.' 
-    DATABASE = 'HayDay_Farm'  
-    
-    connection_string = f"mssql+pyodbc://@{SERVER}/{DATABASE}?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
-    
-    try:
-        print("(с2)... Підключення до сервера SQL Server...")
-        engine = create_engine(connection_string)
-        # transform_base_dimensions(engine)
-        # transform_game_entities(engine)
-        transform_farm_facts(engine)
-
-        print("(с2)...Конвеєр виконано без помилок! Перевіряй таблиці.")
-        
-    except Exception as e:
-        print(f"(с2)...Помилка виконання конвеєра: {e}")
