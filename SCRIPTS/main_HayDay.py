482af671cf6e4b6b8b4e61677b014f346523f543
fesqia <se23ex-02@stu.zsea.edu.ua>
2026-07-04 15:26:58 +0300

    feat: replace standard prints with proper log levels

--- SCRIPTS/main_HayDay.py
+++ SCRIPTS/main_HayDay.py
@@ -1,8 +1,18 @@
+import logging
 from main_pipeline import Hay_Day_ETL_pipeline
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - [%(levelname)s] - %(message)s", 
+    handlers=[
+        logging.FileHandler("pipeline.log", encoding="utf-8"),
+        logging.StreamHandler()  
+    ]
+)
 
 if __name__ == "__main__":
     try:
-        print("(... Підключення до сервера SQL Server...")
+        logging.info("(Підключення до сервера SQL Server.")
 
         pipeline = Hay_Day_ETL_pipeline()
         
@@ -10,7 +20,7 @@ if __name__ == "__main__":
         pipeline.transform_game_entities()
         pipeline.transform_farm_facts()
 
-        print("...Конвеєр виконано без помилок! Перевіряй таблиці.")
+        logging.info("Конвеєр main_pipeline виконано без помилок! Перевіряй таблиці.")
         
     except Exception as e:
-        print(f"(...Помилка виконання конвеєра: {e}")
+        logging.critical(f"(Помилка запуску конвеєра main_pipeline: {e}")
