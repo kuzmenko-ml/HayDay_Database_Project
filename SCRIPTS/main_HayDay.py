30ec5e199cb95e532a0d0591b2a30425bb1eece8
fesqia <se23ex-02@stu.zsea.edu.ua>
2026-07-05 13:15:01 +0300

    refactor: two pipelines have one entry point

--- SCRIPTS/main_HayDay.py
+++ SCRIPTS/main_HayDay.py
@@ -1,5 +1,11 @@
+import os
 import logging
+from raw_etl_pipeline import load_raw_data
 from main_pipeline import Hay_Day_ETL_pipeline
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+load_dotenv()
 
 logging.basicConfig(
     level=logging.INFO,
@@ -11,16 +17,26 @@ logging.basicConfig(
 )
 
 if __name__ == "__main__":
+    server = os.environ.get("DB_SERVER", ".")
+    database = os.environ.get("DB_DATABASE", "HayDay_Farm")
     try:
-        logging.info("(Підключення до сервера SQL Server.")
+        logging.info("--- СТАРТ ФАЗИ 1: Завантаження сирих даних ---")
 
-        pipeline = Hay_Day_ETL_pipeline()
+        connection_string = f"mssql+pyodbc://@{server}/{database}?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes"
+        engine = create_engine(connection_string)
+
+        load_raw_data(engine)
+        logging.info("Фаза 1 успішно завершена.")
+
+        logging.info("--- СТАРТ ФАЗИ 2: Трансформація даних ---")
+
+        pipeline = Hay_Day_ETL_pipeline(server=server, database=database)
         
         pipeline.transform_base_dimensions()
         pipeline.transform_game_entities()
         pipeline.transform_farm_facts()
 
-        logging.info("Конвеєр main_pipeline виконано без помилок! Перевіряй таблиці.")
+        logging.info("Увесь ETL-конвеєр виконано без помилок! Перевіряй таблиці.")
         
     except Exception as e:
-        logging.critical(f"(Помилка запуску конвеєра main_pipeline: {e}")
+        logging.critical(f"Критична помилка виконання конвеєра: {e}")
